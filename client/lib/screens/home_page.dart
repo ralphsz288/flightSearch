2b5eee4a4865276b8c7aaa78d7bbdad35df31def
ralphsz288 <rakph28@gmail.com>
2023-08-05 02:46:16 +0300

    displayed departure data

--- client/lib/screens/home_page.dart
+++ client/lib/screens/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:client/models/flight_state.dart';
-import 'package:client/models/view_state.dart';
 import 'package:client/providers.dart';
-import 'package:client/providers/flight_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:client/screens/flight_search_result_page.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,36 +15,34 @@ class _HomePageState extends ConsumerState<HomePage> {
   late String fromLocation;
   late String toLocation;
 
-
   @override
   Widget build(BuildContext context) {
     FlightState state;
     final viewState = ref.watch(flightStateProvider);
 
     return viewState.when(
-        loading:() => _loading(),
+        loading: () => _loading(),
         ready: () => _body(),
-        error:() => _error(),
+        error: () => _error(),
         completed: (FlightState state) => _completed(state));
-
   }
-    Widget _loading() {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+
+  Widget _loading() {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
 
   Widget _error() {
     return const Scaffold(
       body: Center(
-        child: Scaffold(
-          body: Center(
-            child: Text('There was an error'),
-          ),
-        )
-      ),
+          child: Scaffold(
+        body: Center(
+          child: Text('There was an error'),
+        ),
+      )),
     );
   }
 
@@ -59,16 +54,73 @@ class _HomePageState extends ConsumerState<HomePage> {
             onPressed: () {
               ref.read(flightStateProvider.notifier).setToReady();
               Navigator.of(context).pushNamed('/home');
-            }, icon: const Icon(Icons.arrow_back),
+            },
+            icon: const Icon(Icons.arrow_back),
           ),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(state.fromLocation),
-              Text(state.toLocation),
-              Text(state.resp![0]['data']['price'].toString()),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                child: Card(
+                  elevation: 2,
+                  shadowColor: Colors.red,
+                  color: Colors.white30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            state.fromLocation.toUpperCase(),
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            state.resp![0]['data']['departure_city'],
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          Text(
+                            state.resp![0]['data']['departure_date'].substring(0,10),
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          Text(
+                            state.resp![0]['data']['departure_date'].substring(11,16),
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
+                      const Icon(
+                        Icons.flight_takeoff_rounded,
+                        size: 30,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            state.toLocation.toUpperCase(),
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            state.resp![0]['data']['destination_city'],
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          Text(
+                            state.resp![0]['data']['departure_arrival'].substring(0,10),
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          Text(
+                            state.resp![0]['data']['departure_arrival'].substring(11,16),
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -79,102 +131,105 @@ class _HomePageState extends ConsumerState<HomePage> {
     );
   }
 
-    Widget _body(
-        {FlightState? state}
-        ) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+  Widget _body({FlightState? state}) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 5,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20,top: 5,),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Center(
+                child: Text(
+              'Flight Search',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+            )),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: TextFormField(
+                autofocus: false,
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    labelText: 'From:',
+                    hintText: 'Enter iatacode',
+                    prefixIcon: Icon(Icons.flight_takeoff_outlined),
+                    border: OutlineInputBorder()),
+                onChanged: (value) {
+                  setState(() {
+                    fromLocation = value;
+                  });
+                },
+                validator: (value) {
+                  return value!.isEmpty ? 'Please enter username' : null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: TextFormField(
+                autofocus: false,
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    labelText: 'To:',
+                    hintText: 'Enter iatacode',
+                    prefixIcon: Icon(Icons.flight_land_outlined),
+                    border: OutlineInputBorder()),
+                onChanged: (value) {
+                  setState(() {
+                    toLocation = value;
+                  });
+                },
+                validator: (value) {
+                  return value!.isEmpty ? 'Please enter username' : null;
+                },
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Center(
                   child: Text(
-                    'Flight Search',
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: TextFormField(
-                  autofocus: false,
-                  keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
-                      labelText: 'From:',
-                      hintText: 'Enter iatacode',
-                      prefixIcon: Icon(Icons.flight_takeoff_outlined),
-                      border: OutlineInputBorder()),
-                  onChanged: (value) {
+                'How many days would you like to spend in your destination?',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: RangeSlider(
+                  values: _rangeValues,
+                  max: 50,
+                  divisions: 50,
+                  labels: RangeLabels(
+                    _rangeValues.start.round().toString(),
+                    _rangeValues.end.round().toString(),
+                  ),
+                  onChanged: (RangeValues values) {
                     setState(() {
-                      fromLocation = value;
+                      _rangeValues = values;
                     });
-                  },
-                  validator: (value) {
-                    return value!.isEmpty ? 'Please enter username' : null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  autofocus: false,
-                  keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
-                      labelText: 'To:',
-                      hintText: 'Enter iatacode',
-                      prefixIcon: Icon(Icons.flight_land_outlined),
-                      border: OutlineInputBorder()),
-                  onChanged: (value) {
-                    setState(() {
-                      toLocation = value;
-                    });
-                  },
-                  validator: (value) {
-                    return value!.isEmpty ? 'Please enter username' : null;
-                  },
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 30),
-                child: Center(
-                    child: Text(
-                      'How many days would you like to spend in your destination?',
-                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: RangeSlider(
-                    values: _rangeValues,
-                    max: 50,
-                    divisions: 50,
-                    labels: RangeLabels(
-                      _rangeValues.start.round().toString(),
-                      _rangeValues.end.round().toString(),
-                    ),
-                    onChanged: (RangeValues values) {
-                      setState(() {
-                        _rangeValues = values;
-                      });
-                    }),
-              ),
-            ],
-          ),
+                  }),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton.extended(
-          label: const Text('search flights'),
-          onPressed: () async {
-            await ref.read(flightStateProvider.notifier).init(fromLocation, toLocation, _rangeValues.start.toString(), _rangeValues.end.toString());
-          },
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      );
-    }
-
-
-
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('search flights'),
+        onPressed: () async {
+          await ref.read(flightStateProvider.notifier).init(
+              fromLocation,
+              toLocation,
+              _rangeValues.start.toString(),
+              _rangeValues.end.toString());
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
 }
